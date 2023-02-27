a7431ba3e0bb3e93e7a95d73357deafe9593698b
u0105632 <fabian.winkelmann@wago.com>
2023-02-27 19:55:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,4 +10,5 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
   console.log(`noch ein test `)
   console.log(`das ist noch ein test`)
+  console.log("das ist noch ein test")
 })
