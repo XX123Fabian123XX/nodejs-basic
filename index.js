ed19962c7e5d2cc586e7d1d181063d82179d3482
XX123Fabian123XX <34072682+XX123Fabian123XX@users.noreply.github.com>
2023-03-11 14:47:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,9 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Das ist ein test !!!!!!!')
+  res.send('Das ist ein test !!!!!!!!!!!!')
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-  console.log(`noch ein test `)
-  console.log(`das ist noch ein test`)
-  console.log("das ist noch ein test")
 })
